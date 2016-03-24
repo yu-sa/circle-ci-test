c5dd2207865a97a0bb3ea84d3c3861f828e5c65c
裕司 金沢 <yuji.kanazawa@gu3.co.jp>
2016-03-24 14:55:37 +0900

    circle 修正

---
+++ example/application/tests/test_application.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+import requests
+
+
+def test_example():
+    host = "127.0.0.1:8000"
+    url = 'http://{}/admin/'.format(host)
+    response = requests.get(url)
+    assert response.status_code == 200
+    assert "Error" not in response.text
+
